80b6d202db9045a607fd3d993ac29dc12b4b355f
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-04 14:50:31 -0400

    Initial fullstack project with backend and frontend

---
+++ backend_soundtrackApp/src/main/java/com/example/backend_soundtrackapp/BackendSoundtrackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_soundtrackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSoundtrackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSoundtrackAppApplication.class, args);
+    }
+
+}
---
+++ backend_soundtrackApp/src/test/java/com/example/backend_soundtrackapp/BackendSoundtrackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_soundtrackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSoundtrackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
