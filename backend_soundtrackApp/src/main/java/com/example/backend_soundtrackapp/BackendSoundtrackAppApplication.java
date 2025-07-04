c10515c3c554b0f11736e9d8853b4d8af1b28417
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-04 15:56:07 -0400

    some changes and testing

--- backend_soundtrackApp/src/main/java/com/example/backend_soundtrackapp/BackendSoundtrackAppApplication.java
+++ backend_soundtrackApp/src/main/java/com/example/backend_soundtrackapp/BackendSoundtrackAppApplication.java
@@ -2,12 +2,19 @@ package com.example.backend_soundtrackapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BackendSoundtrackAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendSoundtrackAppApplication.class, args);
     }
 
+    @GetMapping("/api/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
 }
--- backend_soundtrackApp/src/test/java/com/example/backend_soundtrackapp/controller/MainController.java
+++
@@ -1,4 +0,0 @@
-package com.example.backend_soundtrackapp.controller;
-
-public class MainController {
-}
