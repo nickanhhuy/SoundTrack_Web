195ec12340380a3e3b9ee224f1621ef17cd4989b
Vu Anh Huy Nguyen <nguyenvh@sheridancollege.ca>
2025-07-09 15:22:45 -0400

    Used Postman to insert hardcode songs into playlist. Got error 404 because of incorrect path but then I solved it. Check the console log in the view page source after loading the page and the data has been successfully cached from backend but it just showed the genre but missing two other parementers: title and artist of the songs. Trying to figure out the problem, especially in html with the Binding method

--- frontend_soundtrackApp/src/app/home/home.component.ts
+++ frontend_soundtrackApp/src/app/home/home.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { SongService } from '../song.service';
+import { Playlist } from '../song.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  playlists: Playlist[] = [];
 
+  constructor(private songService: SongService) {
+    this.songService.getPlaylistByGenre().subscribe(data => {
+      console.log('Playlists:', data);
+      this.playlists = data;
+    });
+  }
 }
---
+++ frontend_soundtrackApp/src/app/song.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Interface model for a song and a playlist
+export interface Song {
+  id: number;
+  title: string;
+  artist: string;
+  genre: string;
+}
+export interface Playlist {
+  title: string;
+  songs: Song[];
+}
+
+// implement the service method caching the API request from backend
+@Injectable({
+  providedIn: 'root'
+})
+export class SongService {
+  private apiURL = 'http://localhost:8080/api'; 
+  http: HttpClient;
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+  getPlaylistByGenre(): Observable<Playlist[]> {
+    return this.http.get<Playlist[]>(`${this.apiURL}/playlists/genres`);
+  }
+}
